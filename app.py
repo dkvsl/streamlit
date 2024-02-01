32aa6db3415469bf7dfe6895a1597a826dd3d486
Dani Darko <119711983+dkvsl@users.noreply.github.com>
2024-02-01 03:59:04 +0000

    Finish app

--- app.py
+++ app.py
@@ -67,3 +67,54 @@ with tab2:
   if uploaded_data is not None:
     # find numeric features  in the dataframe
     numeric_cols = df.select_dtypes(include='number').columns.tolist()
+ 
+    # add selection-box widget
+    selected_num_col = st.selectbox("Which numeric column do you want to explore?", numeric_cols)
+
+st.header(f"{selected_num_col} - Statistics")
+     
+col_info = {}
+col_info["Number of Unique Values"] = len(df[selected_num_col].unique())
+col_info["Number of Rows with Missing Values"] = df[selected_num_col].isnull().sum()
+col_info["Number of Rows with 0"] = df[selected_num_col].eq(0).sum()
+col_info["Number of Rows with Negative Values"] = df[selected_num_col].lt(0).sum()
+col_info["Average Value"] = df[selected_num_col].mean()
+col_info["Standard Deviation Value"] = df[selected_num_col].std()
+col_info["Minimum Value"] = df[selected_num_col].min()
+col_info["Maximum Value"] = df[selected_num_col].max()
+col_info["Median Value"] = df[selected_num_col].median()
+ 
+info_df = pd.DataFrame(list(col_info.items()), columns=['Description', 'Value'])
+ 
+# display dataframe as a markdown table
+st.dataframe(info_df)
+
+st.header("Histogram")
+fig = px.histogram(df, x=selected_num_col)
+st.plotly_chart(fig, use_container_width=True)
+
+with tab3:
+  if uploaded_data is not None:
+    # find categorical columns in the dataframe
+    cat_cols = df.select_dtypes(include='object')
+    cat_cols_names = cat_cols.columns.tolist()
+ 
+    # add select widget
+    selected_cat_col = st.selectbox("Which text column do you want to explore?", cat_cols_names)
+ 
+    st.header(f"{selected_cat_col}")
+     
+    # add categorical column stats
+    cat_col_info = {}
+    cat_col_info["Number of Unique Values"] = len(df[selected_cat_col].unique())
+    cat_col_info["Number of Rows with Missing Values"] = df[selected_cat_col].isnull().sum()
+    cat_col_info["Number of Empty Rows"] = df[selected_cat_col].eq("").sum()
+    cat_col_info["Number of Rows with Only Whitespace"] = len(df[selected_cat_col][df[selected_cat_col].str.isspace()])
+    cat_col_info["Number of Rows with Only Lowercases"] = len(df[selected_cat_col][df[selected_cat_col].str.islower()])
+    cat_col_info["Number of Rows with Only Uppercases"] = len(df[selected_cat_col][df[selected_cat_col].str.isupper()])
+    cat_col_info["Number of Rows with Only Alphabet"] = len(df[selected_cat_col][df[selected_cat_col].str.isalpha()])
+    cat_col_info["Number of Rows with Only Digits"] = len(df[selected_cat_col][df[selected_cat_col].str.isdigit()])
+    cat_col_info["Mode Value"] = df[selected_cat_col].mode()[0]
+ 
+    cat_info_df = pd.DataFrame(list(cat_col_info.items()), columns=['Description', 'Value'])
+    st.dataframe(cat_info_df)
